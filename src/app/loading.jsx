b949fbfe4b5e066954d6fd009336bdfe3a6ab8ac
aryan dhungana <125738383+uraqta@users.noreply.github.com>
2025-01-25 14:29:50 +0545

    Delete src directory

--- src/app/loading.jsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-    return (
-        
-        <section className="grid w-svw h-svh place-items-center">
-            <h1 className="text-3xl font-tertiary">LOADING</h1>
-        </section>            
-
-    )
-}
--- src/app/page.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="font-secondary">
-      HomePage
-    </main>
-  );
-}
